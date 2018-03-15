680c0389a230584807c7cc094ba7cd317280c498
yualservice <yualservice@users.noreply.github.com>
2018-03-15 20:56:06 +0300

    menu test

--- scripts/palm-menu.js
+++ scripts/palm-menu.js
@@ -1,8 +1,8 @@
 $("div.menu-icon").click(function(){
     $("nav.site-nav").addClass('open-menu');
 });
-$("html").click(function(){
-	if(!$(e.target).closest('div.wrapper').length && $("nav.site-nav").hasClass('open-menu')){
+$("div.menu-icon").click(function(){
+	if($("nav.site-nav").hasClass('open-menu')){
 		$("nav.site-nav").removeClass('open-menu');
 	}
 });
