a3b9235194197e47584d69c921b8124922bb7825
yualservice <yualservice@users.noreply.github.com>
2018-03-15 20:48:11 +0300

    menu test

--- scripts/palm-menu.js
+++ scripts/palm-menu.js
@@ -1,6 +1,8 @@
 $("div.menu-icon").click(function(){
     $("nav.site-nav").addClass('open-menu');
-	$("html").click(function(){
+});
+$("html").click(function(){
+	if($("nav.site-nav").hasClass('open-menu')){
 		$("nav.site-nav").removeClass('open-menu');
-	});
+	}
 });
